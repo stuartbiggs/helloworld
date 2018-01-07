3fc9609476589b23b8cd4324ec947d4df6d1a567
mintyme777 <smbiduk@yahoo.co.uk>
2018-01-07 17:10:53 +0000

    initial commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: stuartbiggs
+ *
+ * Created on 07 January 2018, 17:04
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    cout << "Hello World" << endl;
+    return 0;
+}
+
